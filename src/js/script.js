f553631ae3078fdde755fe711a32324abbac5934
Liza Usova <Liza.usova@gmail.com>
2024-02-23 01:44:19 +0200

    hw_6 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+/* first tusk */
+const animal = prompt('What\'s your favorite animal?');
+const plant = prompt('What is your favorite plant?');
+const subject = prompt('What is your favorite subject?');
+
+console.log('a ' + subject + ' grew out of ' + plant + ' and the ' + animal + ' was liking it.');
+
+
+/* second task */
+const numbers = 77777;
+const number = numbers.toString();
+
+const firstChar = number [0];
+const secondChar = number [1];
+const thirdChar = number [2];
+const fourthChar = number [2];
+const lastChar = number [number.length - 1];
+
+console.log (firstChar + ' ' + secondChar + ' ' + thirdChar + ' ' + fourthChar + ' ' + lastChar);
