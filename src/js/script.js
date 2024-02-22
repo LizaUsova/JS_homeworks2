112c916eb1f01c9152935ccbe3b1b25e9767162a
Liza Usova <Liza.usova@gmail.com>
2024-02-23 01:58:49 +0200

    hw_7 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+/* task #1 */
+let admin
+const name = 'John';
+
+admin = name;
+
+alert(admin);
+
+/* task #2 */
+const planet = 'Earth';
+const firstName = null;
+
+console.log(planet);
+console.log(firstName);
+
+/* task #3 */
+const someVar = '10' + 10;
+console.log(typeof someVar);
+/*если один из операндов операции + является строкой, то второй операнд будет автоматически преобразован в строку*/
