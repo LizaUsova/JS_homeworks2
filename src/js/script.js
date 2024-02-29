c0e36a6b72a74e36fef5ad8af3f6ca56820dd966
Liza Usova <Liza.usova@gmail.com>
2024-02-29 02:42:37 +0200

    hw_9 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const temperatureValue = +prompt('Write the temperature value to convert');
+const celsiusOrFahrenheit = prompt('Write C if you want to convert to Celsius\nWrite F if you want to convert to Fahrenheit');
+
+
+if (celsiusOrFahrenheit === 'F' || celsiusOrFahrenheit === 'f') {
+    alert('Your result is ' + (temperatureValue * 9 / 5 + 32));
+}
+else if (celsiusOrFahrenheit === 'C' || celsiusOrFahrenheit === 'c') {
+    alert('Your result is ' + (temperatureValue - 32) * 5 / 9);
+}
