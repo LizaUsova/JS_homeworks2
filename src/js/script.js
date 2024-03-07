b82fdec449f6b1e805c8e08327a9555ac5717200
Liza Usova <Liza.usova@gmail.com>
2024-03-07 03:02:54 +0200

    hw_16 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const firstNumber = parseFloat(prompt('Write the first number'));
+const secondNumber = parseFloat(prompt('Write the second number'));
+const thirdNumber = parseFloat(prompt('Write the third number'));
+
+const arithmeticMean = (firstNumber+secondNumber+thirdNumber) / 3;
+
+if (isNaN(firstNumber) || isNaN(secondNumber) || isNaN(thirdNumber)) {
+    alert('Please write the correct operand value');
+} else {
+    alert('Your arithmetic mean is ' + arithmeticMean.toFixed(2))}
