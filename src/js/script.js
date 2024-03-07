93dc0aedbdd3ba0d5341486c820877cca88582c2
Liza Usova <Liza.usova@gmail.com>
2024-03-07 02:49:18 +0200

    hw_14 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const firstNumber = parseFloat(prompt('Write the first number'));
+const secondNumber = parseFloat(prompt('Write the second number'));
+
+const sumOfTotal = firstNumber + secondNumber;
+const difference = firstNumber - secondNumber;
+const product = firstNumber * secondNumber;
+const quotient = firstNumber / secondNumber;
+
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert('Please enter a valid value or the first operand');
+} else {
+    alert(firstNumber + '+' + secondNumber + '=' + sumOfTotal + '\n' +
+        firstNumber + '-' + secondNumber + '=' + difference + '\n' +
+    firstNumber + '*' + secondNumber + '=' + product + '\n' +
+    firstNumber + '/' + secondNumber + '=' + quotient);
+}
