18701d433289a72156b5fc8df2aae1a11ecb89b3
Liza Usova <Liza.usova@gmail.com>
2024-03-10 23:36:42 +0200

    hw_19 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let userNumber = prompt('Введіть число:');
+let number = parseFloat(userNumber);
+
+if (!isNaN(number)) {
+    let pluralForm;
+    if (number === 1) {
+        pluralForm = 'рік';
+    } else if (number > 1 && number < 5) {
+        pluralForm = 'роки';
+    } else {
+        pluralForm = 'років';
+    }
+    alert(number + ' ' + pluralForm);
+} else {
+    alert('Це не число');
+}
