58fea7d7f3c6d1cfb7747f139c494fa70597a191
Liza Usova <Liza.usova@gmail.com>
2024-03-11 00:16:30 +0200

    hw_20 parts 1,3,4 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,55 @@
 'use strict';
+
+//1
+/*let x = 10;
+let y = 7;
+
+let compare = x > y ? 'x більше за y' : 'x не більше, ніж y';
+console.log(compare);
+
+//3
+let userNumber = prompt('ввести ціле число');
+let numberLength = userNumber.length;
+let num = parseInt(userNumber);
+
+
+if (!isNaN(num)) {
+    if (num > 0) {
+        if (numberLength === 1) {
+            alert('Число ' + num + ' є однозначним позитивним');
+        } else if (numberLength === 2) {
+            alert('Число ' + num + ' є двозначним позитивним');
+        } else {
+            alert('Число ' + num + ' є трьохзначним або більше позитивним');
+        }
+    } else if (num < 0) {
+        if (numberLength === 2) {
+            alert('Число ' + num + ' є двозначним негативним');
+        } else {
+            alert('Число ' + num + ' є трьохзначним або більше негативним');
+        }
+    } else {
+        alert('Ви ввели нуль');
+    }
+} else {
+    alert('Введено не число');
+}
+
+//4
+let firstNumber = +prompt('Введите перше число');
+let secondNumber = +prompt('Введите друге число');
+let thirdNumber = +prompt('Введите третє число');
+
+if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+    alert('Найбільше з введених чисел є ' + firstNumber);
+} else if (secondNumber > firstNumber && secondNumber > thirdNumber) {
+    alert('Найбільше з введених чисел є ' + secondNumber);
+} else if (thirdNumber > firstNumber && thirdNumber >secondNumber) {
+    alert('Найбільше з введених чисел є ' + thirdNumber);
+} else {
+    alert('ви не ввели жодного числа')
+}
+
+ */
+
+//5
