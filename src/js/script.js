07a2a52f4fbe1bb40ee99cb776a110d76dc01ce4
Liza Usova <Liza.usova@gmail.com>
2024-03-15 23:31:24 +0200

    hw_24 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+
+//1
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+console.log('Сума елементів масиву: ', sum);
+
+
+//2
+let sumSquare = 0;
+for (let x = 0; x < arr.length; x++) {
+    sumSquare += arr[x] ** 2;
+}
+
+console.log('Сума квадратів елементів масиву: ', sumSquare);
