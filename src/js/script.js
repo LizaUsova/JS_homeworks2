a5b8e0df9522180b51cd1e247dab33f80c24a1d6
Liza Usova <Liza.usova@gmail.com>
2024-03-15 23:48:40 +0200

    hw_25 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const number =+prompt('Введіть число');
+
+if (number < 1 || isNaN(number)) {
+    console.log('NaN');
+} else {
+    let divisor = 2;
+
+    while (number % divisor !== 0) {
+        divisor++;
+    }
+
+    console.log('Найменший дільник числа: ', divisor);
+}
