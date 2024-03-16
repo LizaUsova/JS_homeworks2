d3783fe05d1443b66a0fd299a4e08c4b2446581b
Liza Usova <Liza.usova@gmail.com>
2024-03-16 22:58:24 +0200

    hw_27 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+
+function generateKey(length, characters) {
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+console.log(key);
