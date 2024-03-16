b7a2d1d0a52f774af394de23572fc88c8e6bd74f
Liza Usova <Liza.usova@gmail.com>
2024-03-16 23:37:05 +0200

    hw_28 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const userInputNumber = +prompt('Введіть число');
+const userInputDegreeOfNumber = +prompt('У яку ступінь піднести?');
+
+function degreeNumber(userInputNumber, userInputDegreeOfNumber = 1) {
+    if (isNaN(userInputDegreeOfNumber) || isNaN(userInputNumber)) {
+        alert('Будь ласка, введіть числові значення');
+        return;
+    }
+
+    return Math.pow(userInputNumber, userInputDegreeOfNumber);
+}
+
+const result = degreeNumber(userInputNumber, userInputDegreeOfNumber);
+if (result !== undefined) {
+    alert('Результат: ' + result);
+}
