9ac76ef1656f5be9c718469968855aa3865db910
Liza Usova <Liza.usova@gmail.com>
2024-03-18 19:18:21 +0200

    hw_29 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,27 @@
 'use strict';
 
+function padString(str, length, symbol = '*', positionRight = true) {
+
+    if (typeof str !== 'string' || typeof length !== 'number') {
+        return 'помилка: невірні типи аргументів';
+    }
+    if (length < str.length) {
+        return str.substr(0, length);
+    }
+    if (symbol.length !== 1) {
+        return 'помилка: символ повинен бути рядком довжиною 1 символ';
+    }
+
+    const padLength = length - str.length;
+    const padding = symbol.repeat(padLength);
+
+    if (positionRight) {
+        return str + padding;
+    } else {
+        return padding + str;
+    }
+}
+
+console.log(padString('hello', 8));
+console.log(padString('hello', 6, '*', false));
+console.log(padString('hello', 2));
