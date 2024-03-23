4806a615b2faee5503eac1d5f1e1f8a76ba24e3a
Liza Usova <Liza.usova@gmail.com>
2024-03-23 19:02:17 +0200

    hw_30 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const numbers = [1, 2, 3, -1, -2, -3];
+
+const  foo = function (arr) {
+    const result = [];
+    if (!arr.length) return 'array is empty'
+
+    for (let i = 0; i < arr.length; i++) {
+        const isPositive = Math.sign(arr[i]) === 1 || Math.sign(arr[i]) === 0
+        isPositive && result.push(arr[i])
+    }
+
+    return result.length ? result : null;
+}
+
+console.log(foo(numbers));
+console.log(foo([]));
+console.log(foo([-1, -2, -3]));
